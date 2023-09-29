602245ba3fffa747ebc2473ff68ce9e2792ebda6
Jagdish Gadri <jagdish.g@simformsolutions.com>
2023-09-29 20:19:03 +0530

    Basic setup for css files

---
+++ collab-practical/src/components/home-page/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ReactComponent as MobileIcon } from '../../Assets/Laptop.svg'
+import './HomePage.scss'
+const HomePage = () => {
+    return (
+        <div className='homePageWrapper'>HomePage
+            <MobileIcon />
+        </div>
+    )
+}
+
+export default HomePage
---
+++ collab-practical/src/theme/Theme.js
@@ -0,0 +1,16 @@
+const theme = {
+  token: {
+    colorLink: "red",
+    colorPrimary: "black",
+  },
+  components: {
+    Radio: {
+      colorPrimary: "sky-blue",
+    },
+    Button: {
+      borderRadius: 10,
+    },
+  },
+};
+
+export default theme;
