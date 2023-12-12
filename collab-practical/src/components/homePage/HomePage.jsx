e3b98f1b26bb17ece89590577144e932accb3ae1
Jagdish Gadri <jagdish.g@simformsolutions.com>
2023-12-12 09:35:43 +0530

    bigFix: Scroll issue in prototype page due to image width

--- collab-practical/src/components/homePage/HomePage.jsx
+++ collab-practical/src/components/homePage/HomePage.jsx
@@ -1,28 +1,17 @@
-import React, { useState } from 'react'
-import MobileIcon from '../../Assets/Graphic.svg'
-import { ReactComponent as PinkCircle } from "../../Assets/Ellipse 753.svg";
-import LampIcon from "../../Assets/Lamp.png";
-
+import React from 'react'
 import './HomePage.scss'
-import { Button, Switch } from 'antd'
-// import FeatureItem from '../featureItem/FeatureItem'
-import SolidCircle from '../../Assets/SolidCircle'
-import PlanCard from '../planCard/PlanCard'
-import { featuresListData, objectivesListData, plansCardData } from '../../utils/constants'
-import ObjectiveItem from '../objectiveItem/ObjectiveItem';
 import FeaturesPage from '../../pages/featuresPage/FeaturesPage';
 import PrototypePage from '../../pages/prototypePage/PrototypePage';
 import PlansPage from '../../pages/plansPage/PlansPage';
 import ObjectivesPage from '../../pages/objectivesPage/ObjectivesPage';
 const HomePage = () => {
-    const [isBilledYearly, setIsBilledYearly] = useState(true)
     return (
         <>
 
             <FeaturesPage />
-            {/* <PrototypePage />
+            <PrototypePage />
             <PlansPage />
-            <ObjectivesPage /> */}
+            <ObjectivesPage />
 
         </>
     )
